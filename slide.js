355135cd6d97b454905cb6f77e0167bbbfd68a70
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2019-08-27 17:36:52 +0700

    Delete slide.js

--- slide.js
+++
@@ -1,9 +0,0 @@
-<script>
-$('<div class="heytayo"><div><img src="http://cariprediksi.com/wp-content/uploads/2019/07/PJ106-1-compressed.png" style="padding-top:0px;" alt="Togel Online" height="100%" width="100%"></div><div><img src="http://cariprediksi.com/wp-content/uploads/2019/07/PJ107-1-compressed.png" style="padding-top:0px;" alt="Togel Online" height="100%" width="100%"></div><div><img src="http://cariprediksi.com/wp-content/uploads/2019/07/PJ108-compressed.png" style="padding-top:0px;" alt="Togel Online" height="100%" width="100%"></div></div>').insertBefore($('.wrapper2'));
-</script>
-<script type="text/javascript">
-	jQuery(".heytayo").cycle({
-		fx: 'fade',
-		timeout: 4000,
-	});
-</script>
