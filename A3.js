d91bd70720064f5d3c7fd4ef5f2745868213e436
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2019-09-10 17:36:27 +0700

    Update A3.js

--- A3.js
+++ A3.js
@@ -1,10 +1,1 @@
 document.getElementsByClassName("copyright")[0].children[0].innerText = window.location.host;
-
-var node = document.getElementsByTagName('body')[0].childNodes;
-
-for (var i = 0; i < node.length; i++) {
-    if (node[i].nodeValue === '.') {
-        node[i].remove();
-        break;
-    }
-}
