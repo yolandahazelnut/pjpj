7e5eef129a5f5e51118097c1538922e8ce1287d4
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2019-09-15 11:50:57 +0700

    Create A4.js

---
+++ A4.js
@@ -0,0 +1,3 @@
+(document).ready(function() {
+('.sidebar-button').append('<div style="margin-top:5px;padding-bottom: 5px;"><a title="Bukti Jackpot" href="http://167.71.206.247/" target="_blank" rel="nofollow"><img class="btn-img" src="http://linebwin.com/slider/hometogel/livedraw.png"></a></div>');
+});
