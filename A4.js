301bbdc029ffd31f05b2ead9f1dd3ee627ef71bf
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2019-09-15 13:06:10 +0700

    Update A4.js

--- A4.js
+++ A4.js
@@ -1,2 +1,1 @@
 $('.sidebar-button').append('<div style="margin-top:5px;padding-bottom: 5px;"><a title="Bukti Jackpot" href="http://167.71.206.247/" target="_blank" rel="nofollow"><img class="btn-img" src="https://i.ibb.co/p1ccTg5/asd.png"></a></div>');
-}
