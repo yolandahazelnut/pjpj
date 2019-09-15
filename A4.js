4ee4485f15b1094406eb3766b5afaf8ab9a3a6ff
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2019-09-15 13:13:04 +0700

    Update A4.js

--- A4.js
+++ A4.js
@@ -1,1 +1,1 @@
-$('.sidebar-button').append('<div style="margin-top:5px;padding-bottom: 5px;"><a title="Bukti Jackpot" href="http://167.71.206.247/" target="_blank" rel="nofollow"><img class="btn-img" src="https://i.ibb.co/p1ccTg5/asd.png"></a></div>');
+$('.sidebar-button').append('<a class="btn" style="margin-top:5px;padding-bottom: 5px;" title="Bukti Jackpot" href="http://167.71.206.247/" target="_blank" rel="nofollow"><img class="btn-img" src="https://i.ibb.co/p1ccTg5/asd.png"></a>');
