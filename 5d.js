0fd966c57ff0f45290c527b41e39edb1dc90b40c
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-02-06 10:15:43 +0700

    Create 5d.js

---
+++ 5d.js
@@ -0,0 +1,2 @@
+document.evaluate('//*[@id="content2"]/div[2]/div/div[31]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none";
+document.querySelectorAll("a[href='game.php?game=5d ']")[0].remove();
