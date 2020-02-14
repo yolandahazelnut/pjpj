c0c3d58cd08519d0cce81a47aa6eb1cedb0d81e3
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-02-14 15:51:37 +0700

    Update 5d.js

--- 5d.js
+++ 5d.js
@@ -1,4 +1,4 @@
 document.evaluate('//*[@id="content2"]/div[2]/div/div[32]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none";
-document.evaluate('//*[@id="navigation_games"]/ul/li[10]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none";
+// document.evaluate('//*[@id="navigation_games"]/ul/li[10]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style.display = "none";
 document.querySelectorAll("a[href='game.php?game=5d ']")[0].remove();
 document.querySelectorAll("a[href='game.php?game=5D']")[0].remove();
