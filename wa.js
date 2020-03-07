bb21f501bac1c51671503926eee586fc918fea61
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-03-07 12:54:18 +0700

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,17 +1,7 @@
 $(function () {
         $('.floating-wpp').floatingWhatsApp({
             phone: '6282298235553',
-            popupMessage: 'Selamat Datang di PASANGJITU
-<br>
-<br>Nikmati promo-promo menarik dari kami,
-<br>v Bonus New member 20%
-<br>v Bonus cashback casino 15%
-<br>v Bonus setiap deposit 2%
-<br>v Bonus Prize 2 &amp; 3
-<br>v Bonus cashback bandar darat
-<br>v Bonus refferal 1%
-<br>
-<br>Gabung sekarang dan menangkan hadiahnya!',
+            popupMessage: 'Selamat Datang di PASANGJITU',
             showPopup: true,
             position: 'right', // left or right
             autoOpen: false, // true or false
