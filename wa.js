f58ffdb206daccb01f6e73b8644a1fded58d9bc1
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-03-07 13:02:15 +0700

    Update wa.js

--- wa.js
+++ wa.js
@@ -3,7 +3,7 @@ $(function () {
             phone: '6282298235553',
             popupMessage: 'Selamat Datang di PASANGJITU',
             showPopup: true,
-            position: 'right', // left or right
+            position: 'left', // left or right
             autoOpen: false, // true or false
             //autoOpenTimer: 4000,
             message: 'Halo Pasangjitu',
