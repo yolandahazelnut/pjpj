457e71035819aadddcdd2da34315e58315699574
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-03-10 14:17:57 +0700

    Create bandralotre.js

---
+++ bandralotre.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("copyright")[0].children[0].href = 'http://'+window.location.host;
+document.getElementsByClassName("copyright")[0].children[1].innerHTML = window.location.host;
