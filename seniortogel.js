2de0bb5fbbc6a10a9a84833a096fe34bb683189b
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-04-04 13:10:32 +0700

    Update seniortogel.js

--- seniortogel.js
+++ seniortogel.js
@@ -1,3 +1,3 @@
 var newBanner = document.createElement('div');
 newBanner.innerHTML = '<img src="http://syairhati.com/wp-content/uploads/2020/03/Untitled-1.gif" style="width: 100%;">';
-document.getElementsByClassName('wrapper2')[0].after(newNode);
+document.getElementsByClassName('wrapper2')[0].after(newBanner);
