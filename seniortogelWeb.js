f0ef7b9943c2d04bcd048d930940a813074389b7
yolandahazelnut <45751243+yolandahazelnut@users.noreply.github.com>
2020-04-08 17:18:09 +0700

    Update seniortogelWeb.js

--- seniortogelWeb.js
+++ seniortogelWeb.js
@@ -1,3 +1,6 @@
 var style = document.getElementById('footer').style;
 style.backgroundColor = "transparent";
 style.borderTop = "5px solid #ff0000";
+
+var bank = document.evaluate('//*[@id="bank"]/div/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+bank.innerHTML = '<img style="margin-top: -25px;" src="https://cariprediksi.com/wp-content/uploads/2020/04/000.png">';
